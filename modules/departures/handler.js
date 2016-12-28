b5e4bcfc2ba58117be2cfa7eaeeace76d8790b80
Tom Sarduy <tom.sarduy@gmail.com>
2016-12-28 02:58:17 +0000

    Add departures API router

---
+++ modules/departures/handler.js
@@ -0,0 +1,55 @@
+"use strict";
+
+var rp = require('request-promise');
+var parser = require('../common/api-parser');
+var stations = require('../common/stations');
+
+
+function checkStationCode(req, res, next, stationCode){
+
+	var station = stations.getStation(stationCode);
+
+	// if the station is not valid, 404
+	if(!station){
+		return res.status('404').json({error: 'Not Found'});
+	}
+	
+	// attach station code
+	req.station = station;
+	next();
+}
+
+function fetchDepartures(req, res, next) {
+
+	// fetch realtime api using request-promise
+	const options = {
+		uri: 'https://realtime.thetrainline.com/departures/'+ req.station.crs,
+		// parse the response using custom parser
+		transform: (data) => parser.trainServicesParser(data, req.station),
+		json: true
+	};
+
+	rp(options)
+		.then((data) => {
+			req.data = data;
+			next();
+		})
+		.catch((err) => {
+			res.send(err);
+		})
+}
+
+function getDepartures(req, res, next) {
+
+	res.json({
+		origin: req.station,
+		services: req.data
+	});
+}
+
+
+module.exports = {
+	checkStationCode,
+	fetchDepartures,
+	getDepartures
+};
---
+++ modules/departures/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+var departuresHandler = require('./handler');
+
+/* GET departures listing. */
+router.get('/:stationCode', departuresHandler.fetchDepartures, departuresHandler.getDepartures);
+router.param('stationCode', departuresHandler.checkStationCode);
+
+module.exports = router;
